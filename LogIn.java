871eae19022c45b7299221783e3d02225b7e1646
Jose Andres <benjamin.wwe@hotmail.es>
2016-12-19 15:44:18 -0600

    Add files via upload

---
+++ LogIn.java
@@ -0,0 +1,54 @@
+/* Nombre: Jose Andres Lucatero Tenorio
+*  Descripcion: Juego basado en preguntas para reafirmar conocimientos
+*  Fecha: 29-11-16
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+  public class LogIn extends JFrame implements ActionListener
+  {
+	   private JButton boton;
+	   private JLabel titulo;
+
+     public static void main(String[] args)
+     {
+		    LogIn marco = new LogIn();
+        marco.setSize(110, 100);
+        marco.crearGUI();
+        marco.setVisible(true);
+        marco.setTitle("QuizYou");  // Permite crear el titulo del marco
+        marco.getContentPane().setBackground(Color.red); // permite asignarle un color al marco
+		 }
+
+     public void crearGUI()
+     {
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       Container ventana = getContentPane();
+       ventana.setLayout(new FlowLayout());
+
+       titulo = new JLabel("QuizYou");
+     	 titulo.setFont(new Font("Serif", Font.BOLD, 24));
+     	 ventana.add(titulo);
+
+       boton = new JButton("Jugar");
+     	 ventana.add(boton);
+     	 boton.addActionListener(this);
+	   }
+
+	   public void actionPerformed(ActionEvent event)
+     {
+	      String contra, intro;
+        contra="password";
+        intro = JOptionPane.showInputDialog("Introduce password");
+        if (contra==intro)
+        {
+          //instanciar juego en si
+        }
+        else
+        {
+          JOptionPane.showMessageDialog(null, "Password incorrecta");
+        }
+     }
+}
